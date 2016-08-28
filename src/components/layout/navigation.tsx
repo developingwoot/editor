5881e3201ea17c43ebe207f7db3e13ee7536d6df
developingwoot <developingwoot@gmail.com>
2016-08-28 13:20:10 -0400

    update minor menu styling

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 import { Link } from "react-router";
 
+var menuStyle = {
+    "minWidth" : "400px"
+};
+
 export class Navigation extends React.Component<{}, {}> {
     render() {
         return (
-            <ul role="nav" className={"menu align-right"}>
-                <li className={"menu-text"}>Sour Mash</li>
+            <ul role="nav" style={menuStyle} className={"menu align-right"}>
                 <li><Link to="posts">Posts</Link></li>
                 <li><Link to="/">Dashboard</Link></li>
+                <li className={"menu-text"}>Sour Mash</li>
             </ul>
         );
     }
