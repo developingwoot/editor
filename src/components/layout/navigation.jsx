04812856ebab48c6937c8d1363e45ea1642dc79c
TeamWoot <wooten.joseph@gmail.com>
2016-08-31 20:50:01 -0400

    added mocha chai and enzyme for tests

--- src/components/layout/navigation.jsx
+++ src/components/layout/navigation.jsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { Link } from "react-router";
+import { NavItem } from "../inputs/navitem"
 
 export class Navigation extends React.Component {
     render() {
         return (
             <ul role="nav" className={"menu align-right"}>
-                <li><Link to="settings">Settings</Link></li>
+                <li><Link to='settings'>Settings</Link></li>
                 <li><Link to="posts">Posts</Link></li>
                 <li><Link to="/">Dashboard</Link></li>
                 <li className={"menu-text"}>Sour Mash</li>
